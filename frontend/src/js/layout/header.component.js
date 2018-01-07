c68ff01ddbb0629f6f3167e03dc56862bc167273
jorbencas <jorbencas@gmil.com>
2018-01-07 11:19:43 +0100

    Nav-menu change with login

--- frontend/src/js/layout/header.component.js
+++ frontend/src/js/layout/header.component.js
@@ -4,7 +4,8 @@ class AppHeaderCtrl {
 
     this.appName = AppConstants.appName;
     this.currentUser = User.current;
-
+    this.logout = User.logout.bind(User);
+    
     $scope.$watch('User.current', (newUser) => {
       this.currentUser = newUser;
     })
--- frontend/src/js/profile/profile.controller.js
+++ frontend/src/js/profile/profile.controller.js
@@ -3,11 +3,11 @@ class ProfileCtrl {
     'ngInject';
 
     this.profile = profile;
-    //console.log(profile);
+    console.log(profile);
 
     if (User.current) {
-      //console.log(User.current.username);
-      //console.log(this.profile.username);
+      console.log(User.current.username);
+      console.log(this.profile.username);
       this.isUser = (User.current.username === this.profile.username);
     } else {
       this.isUser = false;
